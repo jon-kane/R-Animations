88b2d53ca579c6074bee17c36e3efd78ead2db21
Jonathan <jonathankanestorey@gmail.com>
2020-03-26 22:25:40 -0500

    Update to make mp4 to post

--- Drop-Wave/01-make-animation.R
+++ Drop-Wave/01-make-animation.R
@@ -33,11 +33,18 @@ pblapply(cl = cl, X = 1:length(data_list), FUN = function(i) {
 
 stopCluster(cl)
 
+# Creates a gif using Imagemagick
+# system_args = paste0(
+#   "convert -delay 3 IMAGES/*.png ",
+#   "-colorspace rgb -colorspace srgb -quantize lab -fuzz 3% +dither ",
+#   "-layers optimize ",
+#   "drop-wave.gif"
+# )
+
+# Creates an mp4 using ffmpeg
 system_args = paste0(
-  "convert -delay 3 IMAGES/*.png ",
-  "-colorspace rgb -colorspace srgb -quantize lab -fuzz 3% +dither ",
-  "-layers optimize ",
-  "drop-wave.gif"
+  "ffmpeg -r 30 -f image2 -s 300x300 -i IMAGES/ts%04d.png ",
+  "-vcodec libx264 -crf 25  -pix_fmt yuv420p drop-wave.mp4"
 )
 
 system(system_args)
